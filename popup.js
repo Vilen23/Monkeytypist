e4005eba1be3ae00cfa5849b34fd3fe0574f8c2f
Vilen23 <shivamlather5@gmail.com>
2024-06-11 02:34:54 +0530

    Finally did it

---
+++ popup.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const fetchButton = document.getElementById('btn');
+  const startTypingButton = document.getElementById('type');
+
+  fetchButton.addEventListener('click', fetchHTML);
+  startTypingButton.addEventListener('click', startTyping);
+
+  function fetchHTML() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          func: getHTML
+        },
+        (results) => {
+          console.log("hello");
+          if (results && results[0] && results[0].result) {
+            words = results[0].result;
+            console.log('Fetched words:', words); // Display the fetched words in the console
+          }
+        }
+      );
+    });
+  }
+
+
+  function getHTML() {
+    const wordDiv = document.getElementById('words');
+    const wordDivs = wordDiv.getElementsByClassName('word');
+    const typing = [];
+    for (let i = 0; i < wordDivs.length; i++) {
+      const wordDiv = wordDivs[i];
+      const letterTag = wordDiv.getElementsByTagName('letter');
+      let string = '';
+      for (let i = 0; i < letterTag.length; i++) {
+        string += letterTag[i].textContent.trim();
+      }
+      typing.push(string);
+      typing.push(" ");
+    }
+    document.elementFromPoint(window.innerWidth / 2, window.innerHeight / 2).click();
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < typing.length) {
+        let word = typing[index];
+        for (let i = 0; i < word.length; i++) {
+          let char = word[i];
+          document.execCommand('insertText', false, char);
+        }
+      }
+      index++;
+    }, 100)
+    console.log(typing.length)
+    return typing;
+  };
+})
+
